de8b75e9a096edc7359097a4e0eb62425533d93f
Diego Cohen <diego.cohen3@gmail.com>
2015-05-07 17:15:57 -0400

    added result count message to error log in node player

--- flashplayer/nodeplayer/index.php
+++ flashplayer/nodeplayer/index.php
@@ -28,7 +28,7 @@
 
   $node = node_load($nid);
   $result = views_get_view_result("series_contents", "grid_related_shows", $_SESSION['series']['nid']);
-  echo("<script>console.log($result)</script>");
+  echo("<script>console.log(" + count($result) + ")</script>");
 
   $playlist = $DRUPAL_URL . "/sites/all/themes/artonair/custom_functions/node_playlist_xspf.php?nid=" . $nid;
   $full_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
