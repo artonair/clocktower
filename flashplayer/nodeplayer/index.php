6257ecd4926def462d6180243197f25635855865
Diego Cohen <diego.cohen3@gmail.com>
2015-05-07 17:52:22 -0400

    Removed result count variable in player

--- flashplayer/nodeplayer/index.php
+++ flashplayer/nodeplayer/index.php
@@ -28,8 +28,7 @@
 
   $node = node_load($nid);
   $result = views_get_view_result("series_contents", "grid_related_shows", $_SESSION['series']['nid']);
-  $count = count($result);
-  echo("<script>console.log($count)</script>");
+  echo("<script>console.log($nid)</script>");
 
   $playlist = $DRUPAL_URL . "/sites/all/themes/artonair/custom_functions/node_playlist_xspf.php?nid=" . $nid;
   $full_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
@@ -37,11 +36,11 @@
   $prevnode = -1;
   $nextnode = -1;
 
-  if($count) > 1) {
+  if(count($result) > 1) {
 
-    for($i = 0; $i <= $count; $i++) {
+    for($i = 0; $i <= count($result); $i++) {
       if($nid == $result[$i]->node_node_data_field_included_shows_nid) {
-        if($i < ($count - 1)){
+        if($i < (count($result) - 1)){
           $prevnode = $result[$i+1]->node_node_data_field_included_shows_nid;
           if($result[$i-1]){
             $nextnode = $result[$i-1]->node_node_data_field_included_shows_nid;
