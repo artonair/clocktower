5c7cd1a8d5d0fd24964500f140684278057ddbe4
Diego Cohen <diego.cohen3@gmail.com>
2015-05-14 12:42:44 -0400

    Added logic for setting prev show on last show in series

--- flashplayer/nodeplayer/index.php
+++ flashplayer/nodeplayer/index.php
@@ -50,6 +50,8 @@
           if($result[$i-1]){
             $nextnode = $result[$i-1]->node_node_data_field_included_shows_nid;
           }
+        } elseif($i == count($result) - 1) {
+          $prevnode = $result[$i+1]->node_node_data_field_included_shows_nid;
         }
       }
     }
