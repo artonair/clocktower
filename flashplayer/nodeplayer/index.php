de4408b2f5b5fe29ae834a58c3fbc4e9369dcbf5
Diego Cohen <diego.cohen3@gmail.com>
2015-07-10 15:30:34 -0400

    Console logged nid

--- flashplayer/nodeplayer/index.php
+++ flashplayer/nodeplayer/index.php
@@ -51,8 +51,8 @@
   if($resultcount > 1) {
     for($i = 0; $i < $resultcount; $i++) {
       $showid = $result[$i]->node_node_data_field_included_shows_nid;
-      debug_to_console($showid);
       if($nid == $result[$i]->node_node_data_field_included_shows_nid) {
+        debug_to_console($nid);
         if($i < ($resultcount - 1)){
           $prevnode = $result[$i+1]->node_node_data_field_included_shows_nid;
           if($result[$i-1]){
