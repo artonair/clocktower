008b589411c0662c2d08152d378ce17de3418627
Diego Cohen <diego.cohen3@gmail.com>
2015-07-10 16:37:30 -0400

    Changed view to show all in related items

--- flashplayer/nodeplayer/index.php
+++ flashplayer/nodeplayer/index.php
@@ -37,7 +37,7 @@
 
   $node = node_load($nid);
   $nidd = (node_load($node->field_series[0])->nid);
-  $result = views_get_view_result("series_contents", "grid_related_shows", $nidd);
+  $result = views_get_view_result("series_contents", "block_1", $nidd);
   $resultcount = count($result);
   $playlist = $DRUPAL_URL . "/sites/all/themes/artonair/custom_functions/node_playlist_xspf.php?nid=" . $nid;
   $full_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
