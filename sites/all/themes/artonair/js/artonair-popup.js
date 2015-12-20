357bffabce35cc946d2404f41a3176daa33dc675
Diego Cohen <diego.cohen3@gmail.com>
2015-12-20 11:32:10 -0500

    Added domain path to popup cookie

--- sites/all/themes/artonair/js/artonair-popup.js
+++ sites/all/themes/artonair/js/artonair-popup.js
@@ -2,6 +2,6 @@ $( window ).load(function() {
   var listen = $('#listennow');
   if(!$.cookie('clocktower_stream')) {
     listen[0].click();
-    $.cookie("clocktower_stream", 1, { expires : 1 });
+    $.cookie("clocktower_stream", 1, { expires : 1, path: '/' });
   }
 });
